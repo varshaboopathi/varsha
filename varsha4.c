79549855b96fd84ea20da882205527bbb7c75994
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2017-09-01 17:35:19 +0800

    Create varsha4.c

---
+++ varsha4.c
@@ -0,0 +1,25 @@
+#include<stdio.h.
+void main()
+{
+int a,b,c
+pritnf("enter the numbers");
+scanf("%d,%d,%d",&a,&b,&c);
+if(a>b)
+{
+if(a>c)
+{
+printf("a is big");
+}
+else
+{
+printf("c is big");
+}
+if(b>c)
+{
+printf("b is big");
+}
+else
+{
+printf("c is big");
+}
+}
