c144f0c31679a91284097669cfa3596570f2b1f0
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2017-09-02 09:56:01 +0530

    Create varsha10.c

---
+++ varsha10.c
@@ -0,0 +1,14 @@
+#include<stdio.h.
+void main()
+{
+long long n;
+int count=0;
+printf("enetr an integer"0;
+scanf("%lld",&n);
+while(n!=0)
+[
+n/=10;
+++count;
+]
+printf('number of digits %d',count);
+}
