55bbfc96a2fff76854b551bd40befe9f0d29414a
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2017-09-02 11:53:10 +0530

    Create 22.c

---
+++ 22.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+vint gcd(int a,int b)
+{
+if(a==0||b==0)
+return 0;
+if(a==b)
+returna;
+if(a>b)
+return gcd(a-b,b);
+return gcd(a,b-a);
+}
+int main()
+{
+int a=98,b=56;
+printf("gcd of %d and%/d is %d",a,b,gcd(a,b));
+return 0;
+}
