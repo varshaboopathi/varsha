755909d7f4344d6ba370151f0419b68ff8e5c689
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2017-09-02 10:26:11 +0530

    Create varsha4a.c

---
+++ varsha4a.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char c;
+printf("enter tghe character");
+scanf("%c',&c);
+if((c>='a'&&c<='z')||(c>='A'&&c<='z'0)
+printf("%c is an alphabet",c0;
+else
+printf("%c is not an alphabet",c);
+return 0;
+}
