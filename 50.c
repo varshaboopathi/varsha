cca5ab6ed5bd8db5c3e7a4b6d81133936385c870
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2017-09-03 21:01:02 +0530

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define bool int
+bool is PowerOfTwo(int x)
+{
+return x &&(!(x&(x-1)));
+}
+int main()
+{
+isPowerofTwo(31)?printf("yes"):printf("no");
+isPowerOfTwo("16)printf("yes"):printf("no);
+return 0;
+}
