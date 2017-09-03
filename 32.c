031737ef583cd4f59dbb8d2d213e51682e5ae8b0
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2017-09-03 19:26:47 +0530

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char s[200];
+int count=0,i;
+scanf("%s",&s);
+for(i=0;s[i]!='\0';i++)
+{
+if(s[i]=='')
+count++;
+}
+printf("%d",count+1);
+}
