33dcda7e291732f6dff92f1cb52caa54b3cc0c62
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2017-09-05 16:44:57 +0530

    Create 71.c

---
+++ 71.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+{
+	int num;
+	printf("Enter an integer\n");
+  scanf("%d",&num);
+	printf("integer entered by you is %d\n"	, num);
+	return 0;
+}
