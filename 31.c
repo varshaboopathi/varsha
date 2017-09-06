d368653368ed2b15dd0ab6f6635bf5ceb871cfd0
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2017-09-06 16:51:15 +0530

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int mystrlen(char *s)
+{
+    char s[]=”program”;
+    int counter = 0 ;
+    int i;
+    for (i = 0; i < s[i]!= 0; i++)
+    { 
+        counter++;
+    }
+    printf( "The number of characters is %d", counter);
+    return 0;
